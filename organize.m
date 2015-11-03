9580141f5afcfd38dcb22b474cd9e62036d50d9f
Roxane <rdebruyker@ensc.fr>
2015-11-03 10:47:21 +0100

    Accelerate video processing by processing model only in small interesting zone and prevent the case we have 5 or more labels

--- organize.m
+++ organize.m
@@ -2,7 +2,9 @@ function orga = organize(prevOrg, curOrg)
     %return curOrg which is the curent organization of barycenters classified
     %depending on the previous one
     orga = [];
-    for i=1:size(curOrg(:,1)) % do it for each dot
+    for i=1:size(prevOrg(:,1)) % do it for each dot
+        %        prevOrgRep = repmat(prevOrg(i,:), size(curOrg, 1), 1); % replicate the ith row of the previous organized matrix in order not
+        
         prevOrgRep = repmat(prevOrg(i,:), size(curOrg, 1), 1); % replicate the ith row of the previous organized matrix in order not to loop
         A = [];
         A = abs(curOrg-prevOrgRep); %delta matrix from the prevOrg[i] for each row of the current organization
