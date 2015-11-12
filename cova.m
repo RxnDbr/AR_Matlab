a485f3f88eceaef454ee49b83943d73f9f562c86
RxnDbr <rdebruyker@ensc.fr>
2015-11-12 12:25:18 +0100

    Fixed cova.m

--- cova.m
+++ cova.m
@@ -5,8 +5,11 @@ matCov = [];
 matCov(1,1) = mean((colorVect(:,1)-meanRGB(1)).* (colorVect(:,1)-meanRGB(1)));
 matCov(1,2) = mean((colorVect(:,1)-meanRGB(1)).* (colorVect(:,2)-meanRGB(2)));
 matCov(1,3) = mean((colorVect(:,1)-meanRGB(1)).* (colorVect(:,3)-meanRGB(3)));
-matCov(2,1) = mean((colorVect(:,2)-meanRGB(2)).* (colorVect(:,2)-meanRGB(2)));
-matCov(2,2) = mean((colorVect(:,2)-meanRGB(2)).* (colorVect(:,3)-meanRGB(3)));
-matCov(2,3) = mean((colorVect(:,3)-meanRGB(3)).* (colorVect(:,3)-meanRGB(3)));
+matCov(2,1) = matCov(1,2);
+matCov(2,2) = mean((colorVect(:,2)-meanRGB(2)).* (colorVect(:,2)-meanRGB(2)));
+matCov(2,3) = mean((colorVect(:,2)-meanRGB(2)).* (colorVect(:,3)-meanRGB(3)));
+matCov(3,1) = matCov(1,3);
+matCov(3,2) = matCov(2,3);
+matCov(3,3) = mean((colorVect(:,3)-meanRGB(3)).* (colorVect(:,3)-meanRGB(3)));
 
 end
